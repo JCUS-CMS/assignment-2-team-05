6c9304e94c3774f6b96f01c950f755dcacb42e19
lumpop <2321900298@qq.com>
2020-01-19 19:24:37 +0800

    create page

--- themes/assignment2/page.php
+++ themes/assignment2/page.php
@@ -1,40 +1,28 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Assignment2
- */
-
 get_header();
 ?>
+<div class="container">
+	<div class="main">
+        <?php if(have_posts()) : ?>
+        <?php while(have_posts()): the_post(); ?>
+            <article class="post">
+                <h3>
+                    <?php the_title(); ?>
+                </h3>
+                <?php if(has_post_thumbnail()) : ?>
+                <div class="post-thumbnail">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <?php endif; ?>
+                <?php the_content(); ?>
+            </article> 
+        <?php endwhile; ?>
+        <?php else : ?>
+            <?php echo wpautop('Sorry, No posts were found.'); ?>
+        <?php endif; ?>
+    </div>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+</div> 
+<?php get_footer(); ?>
+  
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();

20af4b4c7a7b838562e2ef78ecaa3e38e141b1e0
lumpop <2321900298@qq.com>
2020-01-19 19:20:21 +0800

    single.php

--- themes/assignment2/single.php
+++ themes/assignment2/single.php
@@ -1,37 +1,42 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Assignment2
- */
-
 get_header();
 ?>
+<div class="container">
+	<div class="main" style="float: left; width: 68%"> 
+        <?php if(have_posts()) : ?>
+        <?php while(have_posts()): the_post(); ?>
+            <article class="post">
+                <h3>
+					<a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </h3>
+				<div class="meta">
+                Created By <?php the_author(); ?> on<?php the_time('F j, Y g:i a'); ?>
+        		</div>
+                <?php if(has_post_thumbnail()) : ?>
+                <div class="post-thumbnail">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <?php endif; ?>
+                <?php the_content(); ?>
+            </article>
+			<br>
+			<a class="button" href="<?php the_permalink(); ?>"> Read More </a> 
+        <?php endwhile; ?>
+        <?php else : ?>
+            <?php echo wpautop('Sorry, No posts were found.'); ?>
+        <?php endif; ?>
+		<?php comments_template(); ?>
+    </div>
+	<div class="sidebar">
+    	<?php if(is_active_sidebar('sidebar')) : ?>
+    	<?php dynamic_sidebar('sidebar'); ?>
+    	<?php endif; ?> 
+		
+	</div> 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="clr"></div>
+</div>
+<?php get_footer(); ?>
+  
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
